c55cd92c52c1fba1d9e5dcd13a087fddf3fa59c5
jojos15 <georgekosmidis1@yahoo.gr>
2020-10-26 22:54:40 +0200

    leftovers

---
+++ src/components/home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Home = () => {
+
+
+    return (
+        <div className="col-3">
+            <nav className="navbar bg-info">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link 1</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link 2</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link 3</a>
+                    </li>
+                </ul>
+
+            </nav>
+        </div>
+    );
+}
+
+export default Home;
